f8b7b7e41948414c08d5e693a1c4d8ffebac3372
Taylor Lilley <lilleyt09@gmail.com>
2021-07-16 03:56:07 -0400

    fixed typo

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workouts", {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
 });
 
 require("./routes/htmlroutes")(app);
